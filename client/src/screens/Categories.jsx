78a58262486525c202aab1c36b11df9d2ccaaa71
Cameron Izadi <cameronizadi@Camerons-MBP.fios-router.home>
2021-08-27 16:30:45 -0400

    Category changes

--- client/src/screens/Categories.jsx
+++ client/src/screens/Categories.jsx
@@ -6,7 +6,7 @@ export default function Categories(props) {
   return (
     <div>
       <h3>Categories</h3>
-      {categories.map((category) => (
+      {categories?.map((category) => (
         <p key={category.id}>{category.name}</p>
       ))}
     </div>
